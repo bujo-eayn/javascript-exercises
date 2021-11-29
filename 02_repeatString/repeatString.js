cb70cc1ecf6034ed3f81079a2ee15db8b5c6ddaf
Bujo Eayn <jobianonyango@gmail.com>
2021-11-29 22:45:59 +0400

    resolved repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let i = 1;
+    let wordNew = "";
+    if (num === 0) {
+        return wordNew;
+    }else if (Math.sign(num) === 1) {
+        do {
+            i = i + 1;
+            wordNew = wordNew + word;
+            // return wordNew;
+        } while (i <= num);
+        return wordNew;
+        
+    }
+    else{
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
