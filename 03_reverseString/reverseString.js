3e6df97e684d09ee5a6dd24597456e27b0f7afe5
Bujo Eayn <jobianonyango@gmail.com>
2021-11-30 17:44:43 +0400

    repeatString resolved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
 const reverseString = function(str) {
-
-};
+    arrNew = str.split('').reverse().join('');
+    // i = 0;
+    // do {
+        
+    // } while (i < arrNew.lenght);
+    return arrNew;
+    
+}
 
 // Do not edit below this line
 module.exports = reverseString;
